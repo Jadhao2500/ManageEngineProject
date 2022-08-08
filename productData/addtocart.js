b9b0e32e156ef9b3d120d8084b87c2aa2715244b
sobebarali <sobebar.ali17@gmail.com>
2022-08-08 12:05:25 +0530

    ok

--- productData/addtocart.js
+++
@@ -1,8 +0,0 @@
-import {navbar} from "../components/navbar.js"
-import{footer} from "../../components/footer.js";
-
-let footerDiv=document.getElementById("footer")
-    footerDiv.innerHTML=footer();
-
-let nav = document.getElementById("navbardiv");
-    nav.innerHTML = navbar();
